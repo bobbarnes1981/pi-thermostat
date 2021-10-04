54869eeb2636ae879887900ada23c3edcf0726d6
Robert Barnes <robertbarnes1981@googlemail.com>
2021-10-04 20:22:49 +0100

    Add simple schedule

--- pi-thermostat/schedule.py
+++ pi-thermostat/schedule.py
@@ -1,7 +1,35 @@
 
+from datetime import datetime
+
 class Schedule(object):
     def __init__(self):
-        pass
+        self.data = {
+            0: 15.0,
+            1: 15.0,
+            2: 15.0,
+            3: 15.0,
+            4: 15.0,
+            5: 15.0,
+            6: 20.0,
+            7: 20.0,
+            8: 20.0,
+            9: 20.0,
+            10: 20.0,
+            11: 20.0,
+            12: 20.0,
+            13: 20.0,
+            14: 20.0,
+            15: 20.0,
+            16: 20.0,
+            17: 20.0,
+            18: 15.0,
+            19: 15.0,
+            20: 15.0,
+            21: 15.0,
+            22: 15.0,
+            23: 15.0,
+        }
     def get_required_temperature(self):
-        return 20.0
+        t = datetime.now().time()
+        return self.data[t.hour]
 
